939310708f95ab8d5ee84514f580b67f5da063bc
Rahul <rahul97811@gmail.com>
2022-03-06 21:03:35 +0530

    Minor Fix

--- lib/thinkific/enrollment.rb
+++ lib/thinkific/enrollment.rb
@@ -5,18 +5,29 @@ module Thinkific
   # {https://developers.thinkific.com/api/api-documentation/#/Enrollments}
   #
   class Enrollment
-    ENROLLMENTS_PATH  = '/enrollments?query[completed]=true'
-
     class << self
 
       # {https://developers.thinkific.com/api/api-documentation/#/Enrollments/getEnrollments}
       def all(opts = {})
-        path, opts = [ENROLLMENTS_PATH, opts]
+        path, opts = [enrollment_path, opts]
 
         response = Thinkific::Connection.get_json(path, opts)
-        response['items']
+
+        enrollments = []
+        page_number = 0
+
+        while page_number <= response['meta']['pagination']['total_pages']
+          response = Thinkific::Connection.get_json(enrollment_path(page_number), opts)
+          enrollments << response['items']
+          page_number += 1
+        end
+
+        return enrollments.flatten
       end
 
+      def enrollment_path(page_number = 1)
+        "/enrollments?page=#{page_number}&limit=250&query[completed]=true"
+      end
     end
   end
 end
